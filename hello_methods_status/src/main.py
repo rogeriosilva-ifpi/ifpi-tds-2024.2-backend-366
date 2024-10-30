c1dfd73a603750678c231d36eaa9be0c07547366
Rogério Silva <rogerio410@gmail.com>
2024-10-30 15:48:18 -0300

    Orientacoes adicionadas

--- hello_methods_status/src/main.py
+++ hello_methods_status/src/main.py
@@ -3,6 +3,10 @@ from sqlmodel import SQLModel
 from .clubes_controller import router as clubes_router
 from .database import get_engine
 
+# Passos para vc BB
+# > pip install -r requirements.txt
+# > fastapi dev src/main.py
+
 app = FastAPI()
 
 # Registrar os Router (controllers)
