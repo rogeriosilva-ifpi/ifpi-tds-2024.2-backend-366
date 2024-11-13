d7cd357ebfe7bc0718d370281dbcb16640082e7d
Rogério Silva <rogerio410@gmail.com>
2024-11-13 15:39:08 -0300

    Renomeação de Variaveis de Ambiente

--- clubes_api/src/database.py
+++ clubes_api/src/database.py
@@ -4,13 +4,13 @@ from decouple import config
 
 def get_engine():
   # Usuário e Senha
-  user = config('USERNAME')
-  password = config('PASSWORD')
+  user = config('DB_USERNAME')
+  password = config('DB_PASSWORD')
   # Nome do Banco de Dados
   db_name = config('DB_NAME')
   # Host e Porta
-  host = config('HOST')
-  port = config('PORT')
+  host = config('DB_HOST')
+  port = config('DB_PORT')
   # Montar a URL para conexão
   return create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db_name}')
   # return create_engine('sqlite:///banco_livros.db')
