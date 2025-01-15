1d0e6124bacd476e014b3de8a47dc78b0c5d2b58
Rogério Silva <rogerio410@gmail.com>
2025-01-15 15:33:16 -0300

    Django Admin

--- django_projects/app_montadoras/core/admin.py
+++ django_projects/app_montadoras/core/admin.py
@@ -1,13 +1,30 @@
 from django.contrib import admin
-from core.models import Montadora
+from core.models import Montadora, ModeloVeiculo, Veiculo
+
+
+class ModeloInline(admin.TabularInline):
+  model = ModeloVeiculo
+  extra = 0
+  fields = ('nome', 'motorizacao', 'em_producao')
+  show_change_link = True
 
 
 @admin.register(Montadora)
 class MontadoraAdmin(admin.ModelAdmin):
-  list_display = ('nome', 'pais', 'ano_fundacao')
+  list_display = ('nome', 'count_veiculos', 'pais', 'ano_fundacao', 'idade')
   search_fields = ('nome', 'pais')
   list_filter = ('pais', )
 
-# TODO: Modelo Veiculos
+  inlines = [ModeloInline]
 
-# admin.site.register(Montadora, MontadoraAdmin)
+# TODO: Modelo Veiculos
+@admin.register(ModeloVeiculo)
+class ModeloVeiculoAdmin(admin.ModelAdmin):
+  list_display = ('nome', 'motorizacao', 'montadora')
+  list_filter = ('motorizacao', 'montadora')
+
+
+@admin.register(Veiculo)
+class VeiculoAdmin(admin.ModelAdmin):
+  list_display = ('modelo',  'cor', 'ano', 'placa',)
+  list_filter = ('modelo', )
--- django_projects/app_montadoras/core/models.py
+++ django_projects/app_montadoras/core/models.py
@@ -1,14 +1,30 @@
+import datetime
 from django.db import models
-from sqlalchemy import false
+from django.contrib import admin
 
 
 class Montadora(models.Model):
-  nome = models.CharField(max_length=128, blank=False, null=False)
-  pais = models.CharField(max_length=128, blank=False, null=False)
-  ano_fundacao = models.PositiveIntegerField()
+  nome = models.CharField(verbose_name= 'Nome',max_length=128, blank=False, null=False)
+  pais = models.CharField(verbose_name='País', max_length=128, blank=False, null=False)
+  ano_fundacao = models.PositiveIntegerField(verbose_name='Ano de Fundação', blank=False, null=False)
+
+  class Meta:
+    verbose_name = 'Montadora'
+    verbose_name_plural = 'Montadoras'
+    ordering = ['-ano_fundacao']
 
   def __str__(self):
     return f'{self.nome} - {self.pais}'
+  
+  @admin.display(description='Anos de Operação')
+  def idade(self):
+    return datetime.date.today().year - self.ano_fundacao
+  
+  @admin.display(description='Qtd Veículos')
+  def count_veiculos(self):
+    return self.veiculos.count()
+  
+  # count_veiculos.short_description = 'Qtd Veículos'
   
 
 MOTOR_CHOICES = (
@@ -22,11 +38,36 @@ MOTOR_CHOICES = (
 class ModeloVeiculo(models.Model):
   nome = models.CharField(max_length=128, null=False, blank=False)
   motorizacao = models.CharField(choices=MOTOR_CHOICES, max_length=5, null=False, blank=False)
+  em_producao = models.BooleanField(default=False, blank=False, null=False)
 
-  # relacionamento
+  # Relacionamento (Many-to-One)
   montadora = models.ForeignKey(Montadora, 
                                 on_delete=models.RESTRICT, 
                                 related_name='veiculos')
+  
+  class Meta:
+    verbose_name = 'Modelo de Veículo'
+    verbose_name_plural = 'Modelos de Veículos'
+    ordering = ['montadora__nome', 'nome']
 
   def __str__(self):
-    return self.nome
+    return f'{self.montadora} - {self.nome}'
+  
+
+class Veiculo(models.Model):
+  placa = models.CharField(max_length=7, null=False, blank=False)
+  cor = models.CharField(max_length=32, null=False, blank=False)
+  ano = models.PositiveIntegerField(null=False, blank=False)
+  km_rodados = models.PositiveIntegerField(null=False, blank=False)
+
+  modelo = models.ForeignKey(ModeloVeiculo, 
+                            on_delete=models.RESTRICT, 
+                            related_name='veiculos')
+  
+  class Meta:
+    verbose_name = 'Veículo'
+    verbose_name_plural = 'Veículos'
+    ordering = ['modelo__montadora__nome', 'modelo__nome']
+  
+  def __str__(self):
+    return f'{self.modelo} - {self.placa}'
